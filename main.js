6b59adf448f8336b4b177b70e67eee7b7468f268
Morningstar <71162971+morniingstarr@users.noreply.github.com>
2023-05-19 23:57:35 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+function start() {
+    document.getElementById('tab1').style.display = '';
+    document.getElementById('tab2').style.display = 'none';
+    document.getElementById('tab3').style.display = 'none';
+    document.getElementById('tab4').style.display = 'none';
+}
+function tab1() {
+    document.getElementById('tab1').style.display = '';
+    document.getElementById('tab2').style.display = 'none';
+    document.getElementById('tab3').style.display = 'none';
+    document.getElementById('tab4').style.display = 'none';
+}
+function tab2() {
+    document.getElementById('tab1').style.display = 'none';
+    document.getElementById('tab2').style.display = '';
+    document.getElementById('tab3').style.display = 'none';
+    document.getElementById('tab4').style.display = 'none';
+}
+function tab3() {
+    document.getElementById('tab1').style.display = 'none';
+    document.getElementById('tab2').style.display = 'none';
+    document.getElementById('tab3').style.display = '';
+    document.getElementById('tab4').style.display = 'none';
+}
+function tab4() {
+    document.getElementById('tab1').style.display = 'none';
+    document.getElementById('tab2').style.display = 'none';
+    document.getElementById('tab3').style.display = 'none';
+    document.getElementById('tab4').style.display = '';
+}
